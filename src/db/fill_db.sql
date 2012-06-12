767a5d62bb270067a61bd63463f9f9afd5f51652
Steven Oliver <oliver.steven@gmail.com>
2012-06-12 10:17:27 -0300

    Be more generic about which DB we're using.
    Development, at the moment is based on Sqlite3, but lets not make it the only one.

--- src/db/fill_db.sql
+++ src/db/fill_db.sql
@@ -15,8 +15,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with balistica. If not, see <http://www.gnu.org/licenses/>.
 
-PRAGMA foreign_keys = ON
-
 --
 --  CALIBERS
 --
