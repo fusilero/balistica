30f6bb078395d7015955507f09514661e8b28005
Steven Oliver <oliver.steven@gmail.com>
2012-06-11 21:28:00 -0300

    Begin filling default list of calibers

--- src/db/fill_db.sql
+++ src/db/fill_db.sql
@@ -15,4 +15,37 @@
 -- You should have received a copy of the GNU General Public License
 -- along with balistica. If not, see <http://www.gnu.org/licenses/>.
 
+PRAGMA foreign_keys = ON;
 
+INSERT INTO calibers VALUES (1, 'R', '.204');
+INSERT INTO calibers VALUES (2, 'R', '.223');
+INSERT INTO calibers VALUES (3, 'R', '.22-250 Rem.');
+INSERT INTO calibers VALUES (4, 'R', '.243 Win.');
+INSERT INTO calibers VALUES (5, 'R', '.25-06 Rem.');
+INSERT INTO calibers VALUES (6, 'R', '.257 Wby. Mag.');
+INSERT INTO calibers VALUES (7, 'R', '6.5mmx55 Swed Mauser');
+INSERT INTO calibers VALUES (8, 'R', '.260 Rem.');
+INSERT INTO calibers VALUES (9, 'R', '.264 Win. Mag.');
+INSERT INTO calibers VALUES (10, 'R', '.270 Win.');
+INSERT INTO calibers VALUES (11, 'R', '.270 Wby. Mag.');
+INSERT INTO calibers VALUES (12, 'R', '.270 WSM');
+INSERT INTO calibers VALUES (13, 'R', '7mm-08 Rem.');
+INSERT INTO calibers VALUES (14, 'R', '.280 Rem.');
+INSERT INTO calibers VALUES (15, 'R', '.280 Rem. Ackley Imp.');
+INSERT INTO calibers VALUES (16, 'R', '7mm Rem. Mag.');
+INSERT INTO calibers VALUES (17, 'R', '.280 Rem.');
+INSERT INTO calibers VALUES (18, 'R', '');
+INSERT INTO calibers VALUES (19, 'R', '');
+INSERT INTO calibers VALUES (20, 'R', '');
+INSERT INTO calibers VALUES (21, 'R', '');
+INSERT INTO calibers VALUES (22, 'R', '');
+INSERT INTO calibers VALUES (23, 'R', '');
+INSERT INTO calibers VALUES (24, 'R', '');
+INSERT INTO calibers VALUES (25, 'R', '');
+INSERT INTO calibers VALUES (26, 'R', '');
+INSERT INTO calibers VALUES (27, 'R', '');
+INSERT INTO calibers VALUES (28, 'R', '');
+INSERT INTO calibers VALUES (29, 'R', '');
+INSERT INTO calibers VALUES (30, 'R', '');
+INSERT INTO calibers VALUES (31, 'R', '');
+INSERT INTO calibers VALUES (32, 'R', '');
