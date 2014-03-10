378a6d00c072e31fffb85651360efed35fbcfbbe
Steven Oliver <oliver.steven@gmail.com>
2014-03-10 15:13:17 -0400

    Create fill_gauges.sql

---
+++ sql/fill_gauges.sql
@@ -0,0 +1,23 @@
+-- Copyright 2014 Steven Oliver <oliver.steven@gmail.com>
+--
+-- This file is part of balística.
+--
+-- balística is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- balística is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with balística. If not, see <http://www.gnu.org/licenses/>.
+
+INSERT INTO gauges VALUES (1, '.410');
+INSERT INTO gauges VALUES (2, '10ga');
+INSERT INTO gauges VALUES (3, '12ga');
+INSERT INTO gauges VALUES (4, '16ga');
+INSERT INTO gauges VALUES (5, '20ga');
+INSERT INTO gauges VALUES (6, '28ga');
